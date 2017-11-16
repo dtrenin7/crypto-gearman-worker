d36eb2221a838ffcf49504f5c6a75e8edce2702a
dtrenin <dtrenin7@gmail.com>
2017-11-16 18:14:11 +0300

    ethereum calls added

--- src/gearman_worker.cpp
+++ src/gearman_worker.cpp
@@ -7,31 +7,60 @@
 #include <string>
 
 #include "crypto.h"
+#include "cgw-ethereum.h"
 
 #define GEARMAN_CHECK(x) if(gearman_failed(x)) { *result_size = 0; *result = GEARMAN_ERROR; return NULL; }
 
-void* worker_fn(gearman_job_st* job, void* context, size_t* result_size, gearman_return_t* result) {
-
+void* worker_create_account(gearman_job_st* job, void* context, size_t* result_size, gearman_return_t* result) {
     const char* workload = (const char*)gearman_job_workload(job);
     size_t workload_size = gearman_job_workload_size(job);
     if(workload && workload_size) {
-        std::string work(workload, workload_size);
-        printf("Workload = %s size = %lu\n", work.c_str(), work.size());
+        std::string password(workload, workload_size);
 
         GEARMAN_CHECK(gearman_job_send_status(job, 0, workload_size));
         // start progress count
 
-        GEARMAN_CHECK(gearman_job_send_data(job, workload, workload_size));
+        CGW::Ethereum eth;
+        std::string account = eth.createAccount(password);
+        GEARMAN_CHECK(gearman_job_send_data(job, account.c_str(), account.length()));
         // send result
 
         GEARMAN_CHECK(gearman_job_send_status(job, workload_size, workload_size));
         // end progress count
     }
-
     *result_size = 0;
     *result = GEARMAN_SUCCESS;
     return NULL;
 }
+
+void* worker_execute_js(gearman_job_st* job, void* context, size_t* result_size, gearman_return_t* result) {
+    const char* workload = (const char*)gearman_job_workload(job);
+    size_t workload_size = gearman_job_workload_size(job);
+    if(workload && workload_size) {
+        std::string command(workload, workload_size);
+
+        GEARMAN_CHECK(gearman_job_send_status(job, 0, workload_size));
+        // start progress count
+
+        CGW::Ethereum eth;
+        std::string response = eth.run(command);
+        GEARMAN_CHECK(gearman_job_send_data(job, response.c_str(), response.length()));
+        // send result
+
+        GEARMAN_CHECK(gearman_job_send_status(job, workload_size, workload_size));
+        // end progress count
+    }
+    *result_size = 0;
+    *result = GEARMAN_SUCCESS;
+    return NULL;
+}
+
+
+#define add_worker_function(fn_name, fn) \
+  result = gearman_worker_add_function(&worker, fn_name, strlen(fn_name), fn, 0);\
+  if(result != GEARMAN_SUCCESS) {\
+      printf("ERROR while adding function %s -> %s\n", fn_name, gearman_worker_error(&worker));\
+      exit(1); }
 
 void *worker_builder( void *ptr ) {
     gearman_worker_st worker;
@@ -39,12 +68,15 @@ void *worker_builder( void *ptr ) {
 
     gearman_worker_set_timeout(&worker, 100);
 
-    const char* fn_name = "reverse";
+    gearman_return_t result;
+    add_worker_function("create_account", worker_create_account);
+    add_worker_function("execute_js", worker_execute_js);
+    /*const char* fn_name = "reverse";
     gearman_return_t result = gearman_worker_add_function(&worker, fn_name, strlen(fn_name), worker_fn, 0);
     if(result != GEARMAN_SUCCESS) {
         printf("ERROR while adding function %s -> %s\n", fn_name, gearman_worker_error(&worker));
         exit(1);
-    }
+    } //*/
 
     gearman_worker_add_server(&worker, "localhost", 4730);
 
@@ -59,12 +91,12 @@ void *worker_builder( void *ptr ) {
     gearman_worker_free(&worker);
 }
 
-#define THREADS 10
+#define THREADS 4
 
 int main(void) {
 
-  generate_keys();
-/*    pthread_t threads[THREADS];
+  //generate_keys();
+    pthread_t threads[THREADS];
     for(int i = 0; i < THREADS; i++)
         if(pthread_create(&threads[i], NULL, worker_builder, NULL)) {
             printf("Can't create thread\n");
